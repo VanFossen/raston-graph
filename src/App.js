deab52d4f5aeff9b84817326c970cfe6245e2978
VanFossen <nvanfossen@gmail.com>
2022-06-27 10:41:29 -0400

    created basic Graph component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Fetch from './Fetch';
+import Graph from "./Graph";
 
 function App() {
   return <div className="App">
     <Fetch />
+    <Graph />
   </div>;
 }
 
---
+++ src/Graph.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import Dygraph from "dygraphs";
+
+function Graph() {
+  return <p>graph goes here</p>;
+}
+
+export default Graph;

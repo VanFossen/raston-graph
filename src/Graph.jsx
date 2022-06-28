58be0be2e9ad6b03f084e2b71753d59382226720
VanFossen <nvanfossen@gmail.com>
2022-06-28 15:10:35 -0400

    reformatting

--- src/Graph.jsx
+++ src/Graph.jsx
@@ -1,62 +1,55 @@
 import React from "react";
-import {Line} from 'react-chartjs-2';
-import { Chart, registerables} from 'chart.js';
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
-function Graph({data}) {
+function Graph({ data }) {
 
-  if(data) {
+  if (data) {
     const fulldata = {
       labels: data.data.x,
-      datasets: [{
-        label: '',
-        data: data.data.y,
-        barThickness: 1,
-        backgroundColor: 'purple',
-        borderColor: 'hsl(30,88%,69%)'
-
-      }]
-    }
+      datasets: [
+        {
+          label: "",
+          data: data.data.y,
+          barThickness: 1,
+          backgroundColor: "purple",
+          borderColor: "hsl(30,88%,69%)",
+        },
+      ],
+    };
 
     const options = {
-      plugins:{
-        legend:{
-          display: false
-        }
+      plugins: {
+        legend: {
+          display: false,
+        },
       },
-      scales:{
-        x:{
-          type: 'linear',
-          title:{
+      scales: {
+        x: {
+          type: "linear",
+          title: {
             display: true,
-            text: "Wavenumber cm-1"
+            text: "Wavenumber cm-1",
           },
         },
-        y:{
-          title:{
+        y: {
+          title: {
             display: true,
-            text: "Absorbance (-ln(I/IO))"
+            text: "Absorbance (-ln(I/IO))",
           },
-        }
-      }
-    }
+        },
+      },
+    };
 
-
-    return(
+    return (
       <div>
-        <Line
-          height={'600'}
-          width={'1000'}
-          data={fulldata}
-          options={options}
-        />
-
+        <Line height={"600"} width={"1000"} data={fulldata} options={options} />
       </div>
     );
   } else {
     return;
   }
-
 }
 
 export default Graph;

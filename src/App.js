16d6b55c980fbbbf35d4cea26f361877896725c6
VanFossen <nvanfossen@gmail.com>
2022-06-29 14:53:07 -0400

    implemented error catching

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Fetch from "./components/Fetch";
 import Graph from "./components/Graph";
 import Form from "./components/Form";
 import Spinner from "./components/Spinner";
+import Error from "./components/Error";
 
 function App() {
   const [data, setData] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const [params, setParams] = useState({
     species: [{ molecule: "CO", mole_fraction: 0.1 }],
     min_wavenumber_range: 1900,
@@ -25,8 +28,17 @@ function App() {
   return (
     <div className="App">
       <Form params={params} setParams={setParams} />
-      <Fetch params={params} setData={setData} setLoading={setLoading} />
+
+      <Fetch
+        params={params}
+        setData={setData}
+        setLoading={setLoading}
+        setError={setError}
+      />
+
       {loading && <Spinner />}
+      {error && <Error />}
+
       <Graph data={data} setLoading={setLoading} />
     </div>
   );

784a8d9c6057208228593f36a944014866dce611
VanFossen <nvanfossen@gmail.com>
2022-06-30 14:06:43 -0400

    changed default values

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 import Fetch from "./components/Fetch";
-import Graph from "./components/Graph";
 import Form from "./components/Form";
 import Spinner from "./components/Spinner";
 import Error from "./components/Error";
+import Dygraphs from "./components/Dygraphs";
 
 function App() {
   const [data, setData] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  /**
+   * Default Radis App Values
+   *   species: [{ molecule: "CO", mole_fraction: 0.1 }]
+   *   min_wavenumber_range: 1900
+   *   max_wavenumber_range: 2300
+   *   tgas: 300
+   *   tvib: null
+   *   trot: null
+   *   pressure: 1.01325
+   *   path_length: 1
+   *   simulate_slit: false
+   *   mode: "absorbance"
+   *   database: "hitran"
+   */
   const [params, setParams] = useState({
-    species: [{ molecule: "CO", mole_fraction: 0.1 }],
+    species: [{ molecule: "CO", mole_fraction: 1 }],
     min_wavenumber_range: 1900,
     max_wavenumber_range: 2300,
-    tgas: 300,
+    tgas: 294.15,
     tvib: null,
     trot: null,
-    pressure: 1.01325,
-    path_length: 1,
+    pressure: 0.0001,
+    path_length: 10,
     simulate_slit: false,
-    mode: "absorbance",
+    mode: "transmittance_noslit",
     database: "hitran",
   });
 
@@ -39,7 +53,7 @@ function App() {
       {loading && <Spinner />}
       {error && <Error />}
 
-      {!loading && <Graph data={data} setLoading={setLoading} />}
+      <Dygraphs data={data} />
     </div>
   );
 }
